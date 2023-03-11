1cb7b38de6973b4ae73da0bb1ac3b33de2fb2ad8
Seyfaro <edenroy@hotmail.co.uk>
2023-03-11 16:40:24 +1000

    Style grid

--- javascript.js
+++ javascript.js
@@ -1,12 +1,17 @@
-//Connect to grid in HTML
+//Connect to grid in HTML, assign it to the 'container' variable
 const container = document.getElementById('grid-container');
 
 //Create the grid
+//Outer loop: uses 'i' to loop 16 times, i.e 16 x16 = 256 cells
+//Inner loop: creates 16 grid cells, to be looped 16 times
 for (let i =0; i < 16; i++){
     for (let j = 0; j < 16; j++){
         //Create a single cell to be repeated the 16 times
+        //Each cell should be a div created in the HTML 
         let cell = document.createElement('div');
+        //Adds a CSS class to the created div. This can be selected in the styles.cc file to style each cell
         cell.classList.add('grid-cell');
+        //
         cell.style.gridRow = i + 1;
         cell.style.gridColumn = j + 1;
 
